1fae75d20f7ee8c8f6176f12c76129f3d1bc6a26
Lisa Swart <arnoudlisa@gmail.com>
2021-01-20 02:11:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ function destructivelyPrependKitten(name) {
 }
 
 function destructivelyRemoveLastKitten() {
-
+  kittens.pop()
+  return kittens
 }
 
 function destructivelyRemoveFirstKitten() {
-
+  kittens.shift()
+  return kittens
 }
 
 function appendKitten(name) {
